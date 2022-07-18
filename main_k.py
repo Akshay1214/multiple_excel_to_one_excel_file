053986eecccd8142a8e084cab9bc8af25e7ce492
Akshay1214 <86911365+Akshay1214@users.noreply.github.com>
2022-07-18 19:37:23 +0530

    Add files via upload

---
+++ main_k.py
@@ -0,0 +1,81 @@
+# Import Libraries
+import openpyxl as xl
+import glob
+import os
+from openpyxl import load_workbook, Workbook
+
+
+def read_files():
+    list2 = []
+    path = os.getcwd()
+    excel_files = glob.glob(os.path.join(path, "*.xlsx"))
+
+    for file in excel_files:
+        wb = load_workbook(file)
+        ws = wb.active
+
+        for col_cells in ws.iter_cols(min_col=0, max_col=ws.max_column):
+            lst1 = []
+            for cell in col_cells:
+                lst1.append(cell.value)
+            list2.append(lst1)
+    write_data(list2)
+
+
+def iter_rows(ws):
+    for row in ws.iter_rows():
+        yield [cell.value for cell in row if cell.value is not None]
+
+
+def write_data(list2):
+    emp_id = []
+    name = []
+    desi = []
+    loc = []
+    for i in list2:
+        if "Emp ID" in i:
+            for temp in i:
+                emp_id.append(temp)
+        if "Name" in i:
+            for temp in i:
+                name.append(temp)
+        if "Location" in i:
+            for temp in i:
+                loc.append(temp)
+        if "Designation" in i:
+            for temp in i:
+                desi.append(temp)
+    print(emp_id)
+    print(desi)
+    print(loc)
+    print(name)
+    write_dataa(emp_id,desi,loc,name)
+
+def write_dataa(emp_id,desi,loc,name):
+    wb = Workbook()
+    ws = wb.active
+
+    start_row = 2
+    start_col = 2
+
+    # for id,des,loca,nam in zip(emp_id,desi,loc,name):
+
+    for i, statN in enumerate (emp_id):
+        ws.cell(row=i + 3, column=1).value = statN
+
+    for i, statN in enumerate (desi):
+        ws.cell(row=i + 3, column=2).value = statN
+
+    for i, statN in enumerate (loc):
+        ws.cell(row=i + 3, column=3).value = statN
+
+    for i, statN in enumerate (name):
+        ws.cell(row=i + 3, column=4).value = statN
+
+    wb.save("Output.xlsx")
+
+if __name__ == '__main__':
+     read_files()
+    # ws = wb.active
+    # sheet1_value = (list(iter_rows(ws)))
+    # print(sheet1_value)
